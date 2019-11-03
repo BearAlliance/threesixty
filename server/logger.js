384453f28c13fc0cf882c873ea7b48da625b5db5
Nick Cacace <NickCacace@gmail.com>
2019-11-03 15:16:13 -0500

    fix: print all arguments to logger functions

--- server/logger.js
+++ server/logger.js
@@ -1,19 +1,24 @@
 import chalk from 'chalk';
 
+function joinArguments(args) {
+  return Array.prototype.join.call(args, ' ');
+}
+
 function log(message) {
   console.log(message);
 }
 
 function info(message) {
-  log(chalk.bgCyan(' INFO ') + ' ' + message);
+  log(`${chalk.bgCyan(' INFO ')} ${joinArguments(arguments)}`);
 }
 
 function error(message) {
-  log(chalk.bgRed(' ERROR ') + ' ' + message);
+  log(`${chalk.bgRed(' ERROR ')} ${joinArguments(arguments)}`);
 }
 
 const logger = {
-  info
+  info,
+  error
 };
 
 export default logger;

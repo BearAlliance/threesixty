6ebc3f282588407877f05b27610137a6c819c2af
Nick Cacace <NickCacace@gmail.com>
2019-11-04 07:55:47 -0500

    feat: persist state between refreshes

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,17 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist';
 
-export default createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
+
+export default store;
+// export default createStore(rootReducer);
